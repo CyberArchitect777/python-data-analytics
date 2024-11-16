ab1d5fab7417d9d1b23ee74dd67e00e2da8d3b44
CyberArchitect777 <169656116+CyberArchitect777@users.noreply.github.com>
2024-11-16 16:38:27 +0000

    feat: start basic evaluation of the f1 spreadsheet

--- data.py
+++ data.py
@@ -1,3 +1,15 @@
 
 import pandas, numpy
 
+f1data = pandas.read_excel("2023f1data.xlsx")
+
+# Display first five rows of data
+
+print(f1data.head())
+
+# Display column names and data types
+
+print(f1data.info())
+
+
+
